4be9e9d3753f4e89c1dcac06ffcf83fd93bec43d
Mark Ronquillo <markronquillo23@gmail.com>
2016-11-08 21:28:26 +0800

    Finish Chapter 7

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,33 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+"""
+If you want to reorder the fields
+"""
+# class QuestionAdmin(admin.ModelAdmin):
+# fields = ['pub_date', 'question_text']
 
-# Register your models here.
-admin.site.register(Question)
+"""
+If you want to group fields into fieldsets
+"""
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['question_text']
+    list_filter = ['pub_date']
+    list_display = ('question_text', 'pub_date', 'was_recently_published')
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date']})
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+
 admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -15,6 +15,10 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
+    was_recently_published.admin_order_field = 'pub_date'
+    was_recently_published.boolean = True
+    was_recently_published.short_description = "Published recently"
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
